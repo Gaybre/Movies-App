102b34f04003dac4338f5b4e6371ab15960d4370
Juan Gaybre Flores <jugaybre@gmail.com>
2021-09-28 16:23:12 -0500

    [Feature][Juan Gaybre] - axios was installed, making custom axios.get in api/movieDB, movieInterface and api key file example

---
+++ src/api/api_key_example.tsx
@@ -0,0 +1,10 @@
+/* 
+1. you need to create the file 'api_key.tsx'
+2. write the constant API_KEY like the following example
+3. change the 'xxxxxxxxxx' for your  own api key
+
+check the documentation to get an api key and feel free to writte me your doubts
+https://www.themoviedb.org/documentation/api
+*/
+
+export const API_KEY= 'xxxxxxxxxx';
